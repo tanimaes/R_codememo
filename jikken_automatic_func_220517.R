d00a38dea4ebc0fe2783f6434546fa11162d18e9
Shinichiro Tanimae <62975525+tanimaes@users.noreply.github.com>
2022-05-19 09:47:46 +0900

    Update jikken_automatic_func_220517.R

--- jikken_automatic_func_220517.R
+++ jikken_automatic_func_220517.R
@@ -10,7 +10,15 @@ library(stringi)
 # ファイルパスを取り出す. ------------------------------------------------------
 files = tibble(fpath = dir("~/Lab_Data/学生実験/2022データ/", full = T))
 
-# エクセルファイルのシートごとに読み込む関数を作る. ------------------------------
+# 標準誤差計算用の関数.
+# 
+se = function (x, na.rm = FALSE ) { 
+  # 標準誤差の関数 
+  n = length ( ! is.na (x)) 
+  sd (x, na.rm) / sqrt (n -1 ) 
+}
+
+# エクセルファイルのシートごとに読み込むための関数. 
 
 sheet1_func = function(file){
   
@@ -27,22 +35,24 @@ sheet1_func = function(file){
            "time" = matches("時間"),
            "mgl" = matches("酸素"),
            "temp" = matches("水温"),
-           "light_c" = matches("光(環境|条件)"),
+           "light" = matches("光(環境|条件)"),
            "remarks" = matches("備考")) |> 
-    mutate(light_c = as.character(stri_trans_nfkd(light_c))) |> 
-    mutate(light_c = case_when(str_detect(light_c, pattern = "[アAa]") ~ "アルミホイル",
-                               str_detect(light_c, pattern = "6") ~ "6ネット",
-                               str_detect(light_c, pattern = "4") ~ "4ネット",
-                               str_detect(light_c, pattern = "2") ~ "2ネット",
-                               str_detect(light_c, pattern = "0") ~ "0ネット")) |> 
-    mutate(across(c(han, sample, light_c), as.factor)) |> 
+    mutate(light = as.character(stri_trans_nfkd(light))) |> 
+    mutate(light = case_when(str_detect(light, pattern = "[アAa]") ~ "アルミホイル",
+                             str_detect(light, pattern = "6") ~ "6ネット",
+                             str_detect(light, pattern = "4") ~ "4ネット",
+                             str_detect(light, pattern = "2") ~ "2ネット",
+                             str_detect(light, pattern = "0") ~ "0ネット")) |> 
+    mutate(across(c(han, sample, light), as.factor)) |> 
     drop_na(han)
   
   # 6 班の 2 日目の班名が D 班になっているので変える.
   if(str_detect(file, pattern = "13_6班")){
     df = df |> mutate(han = factor(6)) 
   }
-  return(df)
+  df |> 
+    mutate(mgl = ifelse(han == 2 & mgl < 2, mgl*10, mgl),
+           mgl = ifelse(han == 2 & mgl > 50, mgl/10, mgl))
 }
 
 sheet2_func = function(file){
@@ -57,29 +67,29 @@ sheet2_func = function(file){
   df =  df |>
     rename("han" = matches("班"),
            "sample" = matches("サンプル|番号"),
-           "light_c" = matches("光環境"),
+           "light" = matches("光環境"),
            "remarks" = matches("備考"),
-           "light" = matches("光量子量")) |> 
-    mutate(light_c = as.character(stri_trans_nfkd(light_c))) |> 
-    mutate(light_c = case_when(str_detect(light_c, pattern = "[アAa]") ~ "アルミホイル",
-                               str_detect(light_c, pattern = "6") ~ "6ネット",
-                               str_detect(light_c, pattern = "4") ~ "4ネット",
-                               str_detect(light_c, pattern = "2") ~ "2ネット",
-                               str_detect(light_c, pattern = "0") ~ "0ネット")) |> 
+           "ppfd" = matches("光量子量")) |> 
+    mutate(light = as.character(stri_trans_nfkd(light))) |> 
+    mutate(light = case_when(str_detect(light, pattern = "[アAa]") ~ "アルミホイル",
+                             str_detect(light, pattern = "6") ~ "6ネット",
+                             str_detect(light, pattern = "4") ~ "4ネット",
+                             str_detect(light, pattern = "2") ~ "2ネット",
+                             str_detect(light, pattern = "0") ~ "0ネット")) |> 
     mutate(across(c(han, sample), as.factor)) 
   
   # A 班の記入ミスを書き換え.
   if(str_detect(file, pattern = "A班5月13")){
-    df = df |> mutate(light_c = ifelse(row_number() > 18, "2ネット", light_c))
+    df = df |> mutate(light = ifelse(row_number() > 18, "2ネット", light))
   }
   
-  N = df |> drop_na(light_c) |> pull(light_c) |> unique() |> length()
-
+  N = df |> drop_na(light) |> pull(light) |> unique() |> length()
+  
   # アルミホイル使用時の光量子量を書いていない班のデータを書き換え.
   if(N != 5){
     df = df |> 
-      add_row(light_c = c("アルミホイル"),
-              light = 0) |> 
+      add_row(light = c("アルミホイル"),
+              ppfd = 0) |> 
       fill(c(han, sample))
   }
   
@@ -93,10 +103,12 @@ sheet2_func = function(file){
   }
   
   df |> 
-    fill(c("han", "light_c", "sample"), .direction = "down") |> 
-    mutate(light_c = as.factor(light_c)) |> 
-    group_by(han, light_c, sample) |> 
-    summarise(light = mean(light), .groups = "drop")
+    fill(c("han", "light", "sample"), .direction = "down") |> 
+    mutate(light = as.factor(light)) |> 
+    group_by(han, light, sample) |> 
+    summarise(ppfd_mean = mean(ppfd),
+              # ppfd_se = se(ppfd),
+              .groups = "drop")
 }
 
 sheet3_func = function(file){
@@ -113,12 +125,12 @@ sheet3_func = function(file){
            "datetime" = matches("実験日"),
            "species" = matches("海藻"),
            "sample" = matches("サンプル"),
-           "weight" = matches("湿重量"),
+           "gww" = matches("湿重量"),
            "vol" = matches("瓶")) |> 
     mutate(across(c(han, sample, species), as.factor),
-           weight =  str_remove_all(weight, pattern = "[a-z]"),
+           gww =  str_remove_all(gww, pattern = "[a-z]"),
            vol = str_remove_all(vol, pattern = "[a-z]")) |> 
-    mutate(across(c(weight, vol), as.double))
+    mutate(across(c(gww, vol), as.double))
   
   # C 班の データフレームを分割.
   if(str_detect(file, pattern = "C班")){
@@ -131,6 +143,8 @@ sheet3_func = function(file){
   return(df)
 }
 
+# 一括作業用の関数.
+
 func_all = function(file){
   
   s1 = sheet1_func(file)
@@ -138,18 +152,12 @@ func_all = function(file){
   s3 = sheet3_func(file)
   
   s1 |> 
-    left_join(s2, by = c("han", "sample", "light_c")) |> 
-    left_join(s3, by = c("han", "sample")) |> 
-    mutate(mgl = ifelse(han == 2 & mgl < 2, mgl*10, mgl),
-           mgl = ifelse(han == 2 & mgl > 50, mgl/10, mgl))
+    left_join(s2, by = c("han", "sample", "light")) |> 
+    left_join(s3, by = c("han", "sample"))
 }
 
-# データの読み込み. ------------------------------------------------------------
-alldata = files |> mutate(data = map(fpath, func_all)) |> unnest()
 
-# 作図.
-alldata |> 
-  ggplot() +
-  geom_line(aes(x = time, y = mgl, color = light_c, group = light_c)) +
-  facet_wrap(vars(han, species), scales = "free")
-
+mgldata = files |> mutate(data = map(fpath, sheet1_func)) |> unnest() |> select(-fpath)
+mgldata = mgldata |> select(-c(remarks))
+lightdata = files |> mutate(data = map(fpath, sheet2_func)) |> unnest() |> select(-fpath)
+seaweeddata = files |> mutate(data = map(fpath, sheet3_func)) |> unnest() |> select(-fpath)
